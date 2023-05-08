8f5295a0a219d6651c46c05c3b8905597392f4dc
Mylo Yu <emdoubleu.yu@gmail.com>
2023-05-08 16:44:32 -0700

    Added .env files and database connections.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("./utils.js");
 require('dotenv').config();
 
 const express = require('express');
@@ -13,9 +14,22 @@ const Joi = require("joi");
 
 const expireTime = 60 * 60 * 1000; //expires after 1 hour  (minutes * seconds * millis)
 
+/* secret information section */
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+
+var {database} = include('databaseConnection');
+
+const userCollection = database.db(mongodb_database).collection('users');
+
+app.use(express.urlencoded({extended: false}));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req,res) => {
+    //await userCollection.insertOne({username: "test", email: "test@gmail.com", password: "pass"});
     res.send("Hello World!");
 });
 
